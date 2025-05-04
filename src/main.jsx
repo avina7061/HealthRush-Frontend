f30c71baac43745324e3b3bf227af9e185e987fe
Avinash Choudhary <avinashchy73@gmail.com>
2025-05-04 16:37:06 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -8,20 +8,18 @@ import { BrowserRouter } from "react-router-dom";
 // Context Providers
 import UserContext from "./context/UserContext.jsx";
 import CaptainContext from "./context/CaptainContext.jsx";
-import { LocationProvider } from "./context/searchContext.jsx";
+
 import { CaptainDataProvider } from "./context/CaptainDataProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserContext>
       <CaptainContext>
-        <LocationProvider>
-          <CaptainDataProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </CaptainDataProvider>
-        </LocationProvider>
+        <CaptainDataProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </CaptainDataProvider>
       </CaptainContext>
     </UserContext>
   </StrictMode>
